c82ef3c9c515e879c27707c18ab69dae821ccb19
mAbadsa <moh22389@gmail.com>
2023-07-03 15:35:28 +0300

    Files Structure

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config({ path: '../.env' });
+
+const app = express();
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import express from "express";
-const app = express();
-app.listen(8000);
-console.log("Hi");
+import app from './app';
+import { port } from './config/server';
+
+app.listen(port, () => {
+  console.log('Server is running on port:', port);
+});
