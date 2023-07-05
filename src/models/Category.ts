b06848e5add55279b65c3c31178b446435615e02
Qusai Issa <qusai.2010.123@hotmail.com>
2023-07-05 14:22:31 +0300

    Category models

---
+++ src/models/Category.ts
@@ -0,0 +1,32 @@
+import {
+  Table,
+  Model,
+  Column,
+  DataType,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
+
+@Table({
+  timestamps: false,
+  tableName: 'categories',
+})
+export class Category extends Model {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  description!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: true,
+  })
+  img!: string;
+}
