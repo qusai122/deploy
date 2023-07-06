e4c47bd1ac4e1d2e5bef89c9beb5d07d7fb35e36
Qusai Issa <qusai.2010.123@hotmail.com>
2023-07-06 19:44:42 +0300

    reat the user_orders table

--- src/models/UserOrder.ts
+++ src/models/UserOrder.ts
@@ -1,7 +1,27 @@
-import { Model, Table } from 'sequelize-typescript';
+import { HasOne, Model, Table, Column, DataType } from 'sequelize-typescript';
+import { Cart } from './Cart';
 
 @Table({
   timestamps: true,
   tableName: 'user_orders',
 })
-export class UserOrder extends Model {}
+export class UserOrder extends Model {
+  @HasOne(() => Cart, 'id')
+  cart_id!: Cart;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: {
+        msg: 'Must be a valid email address',
+      },
+    },
+  })
+  email!: string;
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  status!: string;
+}
