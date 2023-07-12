623559a38991ef8fda86e98be744bee24dc7c9f7
qusai122 <qusai.2010.123@hotmail.com>
2023-07-13 02:36:00 +0300

    Fixing RequestHandler names

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 import {
   getLimitedEdition,
   getProducts,
-  Handpicked,
-  Popular,
-  newArrivals,
+  getHandpicked,
+  getPopular,
+  getNewArrivals,
 } from '@controllers/product';
 const router = Router();
 
 router.get('/', getProducts);
 router.get('/limited', getLimitedEdition);
-router.get('/handpicked', Handpicked);
-router.get('/Popular', Popular);
-router.get('/new', newArrivals);
+router.get('/handpicked', getHandpicked);
+router.get('/Popular', getPopular);
+router.get('/new', getNewArrivals);
 
 export default router;
