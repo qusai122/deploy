09e1e375906b4033bd993dd04d4d75330ef0ef8b
qusai122 <qusai.2010.123@hotmail.com>
2023-07-16 12:14:57 +0300

    get products by brand

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { dbConnection } from '@database/index';
 import ProductRoutes from '@routes/product';
+import BrandRoutes from '@routes/brand';
 
 const app = express();
 
 dbConnection.sync({ alter: false });
 
 app.use('/product', ProductRoutes);
+app.use('/brand', BrandRoutes);
 
 export default app;
