91f6c16d70db70066bacf0b42ab9e8515d93fa01
mAbadsa <moh22389@gmail.com>
2023-07-16 19:35:41 +0300

    delete peorducrs route

--- src/routes/products.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { ProductsByBrandQueryDto } from '@dtos/products.dto';
-import { validationMiddleware } from '@middlewares/validation';
-
-const router = Router();
-
-router.route('/').get(validationMiddleware(ProductsByBrandQueryDto, 'query'));
